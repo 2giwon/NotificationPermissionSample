03f77c229565576afb9a35e5971bb6c17828647c
middlefgr <2giwon@gmail.com>
2022-09-25 22:52:30 +0900

    [수정] Android 13 대응

--- app/src/main/java/com/egiwon/notificationpermissiontest/MainActivity.kt
+++ app/src/main/java/com/egiwon/notificationpermissiontest/MainActivity.kt
@@ -1,15 +1,71 @@
 package com.egiwon.notificationpermissiontest
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            createChannel(getString(R.string.notification_channel_id))
+            registerFCM()
+        } else {
+            Toast.makeText(this@MainActivity, "알림을 표시하지 않음", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            askNotificationPermission()
+        } else {
+            createChannel(getString(R.string.notification_channel_id))
+            registerFCM()
+        }
+
+    }
+
+    private fun createChannel(channelId: String, channelName: String = "Notification") {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+                .apply {
+                    setShowBadge(false)
+                }
+
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = getString(R.string.reminder)
+
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            )
+
+            notificationManager.createNotificationChannel(notificationChannel)
+
+        }
+    }
+
+    private fun registerFCM() {
         FirebaseMessaging.getInstance().token
             .addOnCompleteListener { task ->
                 if (!task.isSuccessful) {
@@ -22,7 +78,27 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 val token = task.result
+                println("token : $token")
                 Toast.makeText(this@MainActivity, token, Toast.LENGTH_LONG).show()
             }
+    }
+
+    private fun askNotificationPermission() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            when {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                        PackageManager.PERMISSION_GRANTED -> {
+                    createChannel(getString(R.string.notification_channel_id))
+                    registerFCM()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                    // 사용자에게 알림을 활성화하도록 미리 안내해줌
+                }
+                else -> {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+        }
     }
 }
